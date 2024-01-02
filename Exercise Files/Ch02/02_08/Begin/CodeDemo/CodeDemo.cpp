356f6aba4e7396b8d2f5982386069ffea37a9f4c
Mayank-Patel1015 <141281241+Mayank-Patel1015@users.noreply.github.com>
2024-01-02 18:33:51 -0500

    strings and manipulation

--- Exercise Files/Ch02/02_08/Begin/CodeDemo/CodeDemo.cpp
+++ Exercise Files/Ch02/02_08/Begin/CodeDemo/CodeDemo.cpp
@@ -1,11 +1,26 @@
-// Learning C++ 
+// Learning C++
 // Exercise 02_08
-// Strings, by Eduardo Corpeño 
+// Strings, by Eduardo Corpeño and Mayank Patel
 
 #include <iostream>
+#include <string>
+#include <cstring>
 
 using namespace std;
 
-int main(){
+const int LENGTH1 = 25;
+char array_str1[LENGTH1] = "Hello, World! ";
+char array_str2[] = "Hello, Bob!";
+
+string std_str1 = "Hello, std! ";
+string std_str2 = "What's popping? ";
+
+int main()
+{
+    // mutates array_str1
+    strcat(array_str1, array_str2);
+    cout << array_str1 << endl;
+    // doesn't mutate
+    cout << std_str1 + std_str2 << endl;
     return (0);
 }
