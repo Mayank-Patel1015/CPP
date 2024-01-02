fc1267ad53664155b164042f5c31eeca27918e6a
Mayank-Patel1015 <141281241+Mayank-Patel1015@users.noreply.github.com>
2024-01-01 21:28:41 -0500

    01_04

--- Exercise Files/Ch01/01_03/Begin/CodeDemo/CodeDemo.cpp
+++ Exercise Files/Ch01/01_03/Begin/CodeDemo/CodeDemo.cpp
@@ -1,10 +1,11 @@
 // Learning C++
 // Exercise 01_03
-// Hello World, by Eduardo Corpeño
+// Hello World, by Mayank Patel
 
 #include <iostream>
 
 int main()
 {
   std::cout << "Hello World!" << std::endl;
+  return (0);
 }
--- Exercise Files/Ch01/01_04/CodeDemo/CodeDemo.cpp
+++ Exercise Files/Ch01/01_04/CodeDemo/CodeDemo.cpp
@@ -1,15 +1,17 @@
-// Learning C++ 
+// Learning C++
 // Challenge 01_04
-// Console Interaction, by Eduardo Corpeño 
+// Console Interaction, by Mayank Patel
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int main(){
+int main()
+{
 	string str;
+	cout << "Enter your name: ";
 	cin >> str;
-	cout << str;
-	return(0);
+	cout << "Hello, " << str;
+	return (0);
 }
