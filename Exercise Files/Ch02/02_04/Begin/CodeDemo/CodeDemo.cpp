bfeeca89e844a4ab373e307b01ceedb254dfdfeb
Mayank-Patel1015 <141281241+Mayank-Patel1015@users.noreply.github.com>
2024-01-02 15:56:24 -0500

    .

--- Exercise Files/Ch02/02_04/Begin/CodeDemo/CodeDemo.cpp
+++ Exercise Files/Ch02/02_04/Begin/CodeDemo/CodeDemo.cpp
@@ -1,12 +1,26 @@
-// Learning C++ 
+// Learning C++
 // Exercise 02_04
-// Type inference with auto, by Eduardo Corpeño 
+// Type inference with auto, by Eduardo Corpeño
 
 #include <iostream>
 #include <typeinfo>
 
 using namespace std;
 
-int main(){
+auto a = 9;
+auto b = 12345678901;
+auto c = 3.14;
+auto d = 3.14f;
+auto e = false;
+auto f = 'd';
+
+int main()
+{
+    cout << typeid(a).name() << endl;
+    cout << typeid(b).name() << endl;
+    cout << typeid(c).name() << endl;
+    cout << typeid(d).name() << endl;
+    cout << typeid(e).name() << endl;
+    cout << typeid(f).name() << endl;
     return (0);
 }
